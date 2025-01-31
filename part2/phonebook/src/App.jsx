9470d90709e4a8348fcd14c42e7f2fc3475392ba
jvinjam <jvinjam@gmail.com>
2025-01-31 15:47:27 -0500

    part2 exercises - phonebook : 2.16 & 2.17 changes Add notification/error message

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,57 +4,88 @@ import PersonForm from './components/PersonForm'
 import Person from './components/Person'
 import personService from './services/persons'
 
+import './App.css'
+
+//component for search
 const Filter = ({ searchStr, handleMethod }) => <div>filter shown with <input value={searchStr} onChange={handleMethod} /></div>
+
+const Notification = ({ isError, message }) => {
+  if (message === null || message.length == 0) {
+    return null
+  }
+
+  return (
+    <div className={isError ? 'error' : 'notification'} >
+      {message}
+    </div >
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState({ isError: true, message: '' })
 
+  //fetch initial data
   useEffect(() => {
     personService.getAll()
       .then(initialData => setPersons(initialData))
-      .catch(error => console.log('error : ', error.response.statusText))
+      .catch(error => {
+        //console.log('error : ', error.response.statusText)
+        setErrorMessage({ isError: true, message: error.response.statusText })
+        setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+      })
   }, [])
 
   const handleNewPerson = (event) => setNewName(event.target.value)
   const handleNewNumber = (event) => setNewNumber(event.target.value)
   const handleFilter = (event) => setFilter(event.target.value)
 
+  //filter data using search string
   const filteredPersons = filter.length > 0 ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
     : persons
 
+  //add button function  
   const addPerson = (event) => {
     event.preventDefault()
+    //check if person name exists on the server
     const pObj = persons.find(p => p.name === newName)
-    if (pObj != undefined && pObj.number == newNumber) {
+    //if name exists and newNumber is same as current number, alert the user and return
+    if (pObj !== undefined && pObj.number === newNumber) {
       alert(`${newName} is already added to phonebook`)
-      setNewName('')
-      setNewNumber('')
-      return
-    } else if (pObj != undefined && pObj.number != newNumber) {
+    } else if (pObj !== undefined && pObj.number !== newNumber) {
+      //if name exists and a newNumber is entered
+      //if number field is empty, alert the user and return
       if (newNumber.length == 0) {
         alert('Please enter the number.')
         return
       }
+      //ask the user for confirmation to change the entry on the server
       if (confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        //update if user confirms, if not clear the fields and return
         const updatedObj = { ...pObj, number: newNumber }
         updatePerson(pObj.id, updatedObj)
       }
-      setNewName('')
-      setNewNumber('')
-      return
-    }
-    if (newName.length > 0 && newNumber.length > 0) {
+    } else if (newName.length > 0 && newNumber.length > 0) {
+      //create a new entry if a new name and number entered
       const newObj = {
         name: newName,
         number: newNumber
       }
 
+      //add the new entry to the server and rerender
       personService.create(newObj)
-        .then(returnedPerson => setPersons(persons.concat(returnedPerson)))
-        .catch(error => console.log('error : ', error.response.statusText))
+        .then(returnedPerson => {
+          setErrorMessage({ isError: false, message: `Added ${returnedPerson.name}` })
+          setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+          setPersons(persons.concat(returnedPerson))
+        })
+        .catch(error => {
+          setErrorMessage({ isError: true, message: error.response.statusText })
+          setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+        })
     }
     setNewName('')
     setNewNumber('')
@@ -62,21 +93,38 @@ const App = () => {
 
   const updatePerson = (id, personObj) => {
     personService.update(id, personObj)
-      .then(returnedPerson => setPersons(persons.map(p => p.id == returnedPerson.id ? returnedPerson : p)))
-      .catch(error => console.log('error : ', error.response.statusText))
+      .then(returnedPerson => {
+        setErrorMessage({ isError: false, message: `Phone number updated for ${returnedPerson.name}` })
+        setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+        setPersons(persons.map(p => p.id == returnedPerson.id ? returnedPerson : p))
+      })
+      .catch(error => {
+        setErrorMessage({ isError: true, message: `Information for ${personObj.name} was not found` })
+        setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+        setPersons(persons.filter(p => p.id != personObj.id))
+      })
   }
 
   const deletePerson = (person) => {
     if (confirm(`Delete ${person.name}?`)) {
       personService.deleteById(person.id)
-        .then(deletedPerson => setPersons(persons.filter(p => p.id != deletedPerson.id)))
-        .catch(error => console.log('error : ', error.response.statusText))
+        .then(deletedPerson => {
+          setErrorMessage({ isError: false, message: `Deleted ${deletedPerson.name}` })
+          setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+          setPersons(persons.filter(p => p.id != deletedPerson.id))
+        })
+        .catch(error => {
+          setErrorMessage({ isError: true, message: `Information of ${person.name} has already been removed from server` })
+          setTimeout(() => setErrorMessage({ isError: false, message: null }), 5000)
+          setPersons(persons.filter(p => p.id != person.id))
+        })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification isError={errorMessage.isError} message={errorMessage.message} />
       <Filter searchStr={filter} handleMethod={handleFilter} />
       <h3>Add a new</h3>
       <PersonForm addPerson={addPerson} newName={newName} handleNewPerson={handleNewPerson} newNumber={newNumber}
